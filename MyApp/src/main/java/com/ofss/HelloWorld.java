e8543b853f9a53415c644f300db49524b1023d01
saswatparida1 <saswat.parida1@gmail.com>
2020-10-15 14:38:25 +0530

    My First Commit

---
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.ofss;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first git java");
+
+	}
+
+}
